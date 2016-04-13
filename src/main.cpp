719583724a9ab7de4ef53909114ac2829ff1c096
Christopher Bero <bigbero@gmail.com>
2016-04-12 19:05:55 -0500

    added photoresistor for ambient light sensing

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,32 @@
 #include <Arduino.h>
 #include <avr/sleep.h>
 
+/*
+ * Pin Mappings
+ */
+unsigned short int pinLightSensor = A0;
+unsigned short int pinPowerLed = 13;
+unsigned short int pinMotionInterrupt = 2;
+unsigned short int pinSignCS = 12;
+unsigned short int pinSignWR = 11;
+unsigned short int pinSignDATA = 10;
+
+/*
+ * Global Variables
+ */
 int wd;
 char text [] = "ctag";
-const char lumino[] = "123456789abcdefg";
+unsigned short int lightLevel;
+unsigned int fadeDelay = 150;
+//const char lumino[] = "123456789abcdefg";
+
 //#define DEBUG
 
 void blink_pwr()
 {
-  digitalWrite(13, HIGH);
+  digitalWrite(pinPowerLed, HIGH);
   delay(100);
-  digitalWrite(13, LOW);
+  digitalWrite(pinPowerLed, LOW);
 }
 
 void IRQ0 ()
@@ -27,21 +43,21 @@ int main()
 {
   // Setup
   init(); // masked in normal setup()
-  HT1632.begin(12,11,10); // Connect to matrix. CS, WR, DATA
+  HT1632.begin(pinSignCS, pinSignWR, pinSignDATA); // Connect to matrix. CS, WR, DATA
   wd = HT1632.getTextWidth(text, FONT_8X4_END, FONT_8X4_HEIGHT);
   #ifdef DEBUG
     Serial.begin(9600);
   #endif
-  pinMode(2, INPUT);
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
-  digitalWrite(12, HIGH);
+  pinMode(pinMotionInterrupt, INPUT);
+  pinMode(pinPowerLed, OUTPUT);
+  //pinMode(12, OUTPUT);
+  //digitalWrite(12, HIGH);
 
 while (1)
 {
   #ifdef DEBUG
     Serial.println("Running loop.");
-    digitalWrite(13, HIGH);
+    digitalWrite(pinPowerLed, HIGH);
   #endif
 
   // Setup screen
@@ -52,34 +68,42 @@ while (1)
   HT1632.render();
 
   // Fade in
-  for (unsigned short int l = 2; l <= 16; l++)
+  for (lightLevel = 2; lightLevel <= 16; lightLevel++)
   {
-    delay(100);
-    HT1632.setBrightness(l);
+    delay(fadeDelay);
+    HT1632.setBrightness(lightLevel);
+    if ((lightLevel*60) >= (analogRead(pinLightSensor)))
+    {
+      break;
+    }
   }
 
   delay(500);
   // Keep lit while motion is detected
-  while (digitalRead(2) == HIGH)
+  while (digitalRead(pinMotionInterrupt) == HIGH)
   {
     delay(100);
   }
   delay(500);
 
   // Fade out
-  for (unsigned short int l = 16; l > 0; l--)
+  for (; lightLevel > 0; lightLevel--)
   {
-    HT1632.setBrightness(l);
-    delay(100);
+    HT1632.setBrightness(lightLevel);
+    delay(fadeDelay);
   }
   HT1632.clear();
   HT1632.render();
 
   #ifdef DEBUG
-    digitalWrite(13, LOW);
+    digitalWrite(pinPowerLed, LOW);
   #endif
 
   delay(2000);
+
+  if (digitalRead(pinMotionInterrupt) == HIGH)
+    continue;
+
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
   sleep_enable();
   attachInterrupt(0, IRQ0, RISING);
